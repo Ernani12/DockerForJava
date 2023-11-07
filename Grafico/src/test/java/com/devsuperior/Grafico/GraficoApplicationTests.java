f6afaaa0b7c7cf2497e8348cefe816c94d621d06
Ernani12 <39600025+Ernani12@users.noreply.github.com>
2023-11-07 16:16:53 -0300

    Docker , marven , build image with spring boot, artifactiD

---
+++ Grafico/src/test/java/com/devsuperior/Grafico/GraficoApplicationTests.java
@@ -0,0 +1,15 @@
+package com.devsuperior.Grafico;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+import com.example.Grafico.GraficoApplication;
+
+@SpringBootTest (classes=GraficoApplication.class)
+class GraficoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
